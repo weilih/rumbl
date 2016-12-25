951df96fdb8bdf17d469b39c607f4a7fc36c7a33
willieLOH <willie.loh@live.com>
2016-12-25 18:01:02 +0800

    using channels

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -10,9 +10,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -20,9 +20,17 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
